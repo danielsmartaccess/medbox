d7359d9a784b55e7d7d82dd55e5cae6a3f46ce3e
Gabriel Caravantes <gabrielpcaravantes@gmail.com>
2024-11-14 10:52:28 -0300

    ajuste nos imports e no navigator da tela de medicamentos

--- frontend/App.js
+++ frontend/App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import MainScreen from './screens/MainScreen';
-import MedicinesScreen from './Medbox/screens/MedicinesScreen';
+import MedicineScreen from './Medbox/screens/MedicineScreen';
 import ScheduleScreen from './screens/ScheduleScreen';
 
 const Stack = createStackNavigator();
@@ -14,7 +14,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="Medicines" component={MedicinesScreen} />
+        <Stack.Screen name="Medicine" component={MedicineScreen} />
         <Stack.Screen name="Schedule" component={ScheduleScreen} />
       </Stack.Navigator>
     </NavigationContainer>
